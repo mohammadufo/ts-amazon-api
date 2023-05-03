8320f8c56c55d4cea63af95aa0599f227a940064
mohammadufo <shadow.m.s.313@gmail.com>
2023-05-03 12:18:03 +0330

    get single product

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,25 @@
-import express, { Request, Response } from 'express'
-import { sampleProducts } from './data'
-import cors from 'cors'
+import express, { Request, Response } from "express";
+import { sampleProducts } from "./data";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
 app.use(
   cors({
     credentials: true,
-    origin: ['http://localhost:5173'],
+    origin: ["http://localhost:5173"],
   })
-)
+);
 
-app.get('/api/products', (req: Request, res: Response) => {
-  res.json(sampleProducts)
-})
+app.get("/api/products", (req: Request, res: Response) => {
+  res.json(sampleProducts);
+});
 
-const PORT = 4000
+app.get("/api/products/:slug", (req: Request, res: Response) => {
+  res.json(sampleProducts.find((x) => x.slug === req.params.slug));
+});
+
+const PORT = 4000;
 app.listen(PORT, () => {
-  console.log(`server started at http://localhost:${PORT}`)
-})
+  console.log(`server started at http://localhost:${PORT}`);
+});
