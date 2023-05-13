98a392fbf3d99f979043dd37ceb7520816ec4e64
mohammadufo <shadow.m.s.313@gmail.com>
2023-05-13 17:01:00 +0330

    add order route-

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import { productRouter } from "./routers/productRouter";
 import { seedRouter } from "./routers/seedRouter";
 import { userRouter } from "./routers/userRouter";
+import { orderRouter } from "./routers/orderRouter";
 
 const app = express();
 dotenv.config();
@@ -32,6 +33,7 @@ app.use(
 
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
+pp.use("/api/orders", orderRouter);
 app.use("/api/seed", seedRouter);
 
 const PORT = 4000;
